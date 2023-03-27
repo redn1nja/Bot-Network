1e218c7d8d8aa237ef16c963bc85498a05eb488e
redn1nja <ostap.seryvko@ucu.edu.ua>
2023-03-27 14:18:43 +0300

    now it works atleast

--- client.py
+++ client.py
@@ -4,10 +4,9 @@ import requests
 
 class Client:
     ID = 0
-
     def __init__(self, address=None, requests=None) -> None:
-        self.__id = ID
-        ID += 1
+        self.__id = Client.ID
+        Client.ID += 1
         self.__attack_address = address
         self.__own_address = socket.gethostbyname(socket.gethostname())
         self.__requests = requests
@@ -24,18 +23,28 @@ class Client:
 
     def __send_request(self):
         r = requests.get(self.__attack_address)
-        return r.return_code, r.elapsed
+        return r.status_code, r.elapsed
 
     def start_requesting(self):
         while (self.__requests):
             status, time = self.__send_request()
-            if status not in self.__data.keys:
+            if status not in self.__data.keys():
                 self.__data[status] = [0, 0]
             self.__data[status][0] += 1
-            self.__data[status][1] += time
+            self.__data[status][1] += int(str(time).split(".")[-1])
             self.__requests -= 1
 
     def send_datum(self):
         data = self.__data
         self.__data = {}
         return data
+
+
+
+if __name__ == "__main__":
+    c = Client()
+    c.set_address("http://0.0.0.0:8000") #python3 -m http.server
+    c.set_requests(15)
+    c.start_requesting()
+    data = c.send_datum()
+    print(data)
