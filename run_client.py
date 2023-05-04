e36d2c1dad4ce4f54cfff71719457bc47f09cdf7
redn1nja <ostap.seryvko@ucu.edu.ua>
2023-05-04 18:11:30 +0300

    moved everything into root project directory

--- run_client.py
+++
@@ -1,7 +0,0 @@
-from client import Client
-
-if __name__ == "__main__":
-    server_address = ("0.0.0.0", 8000)
-    client = Client(":".join([str(x) for x in server_address]))
-    print("client started")
-    client.run()
