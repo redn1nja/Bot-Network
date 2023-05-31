15b852fd5357b42c71a3c7513e510443891e8295
redn1nja <ostap.seryvko@ucu.edu.ua>
2023-06-01 02:15:59 +0300

    silly little 0 throughput

--- src/client/src/lib.rs
+++ src/client/src/lib.rs
@@ -1,34 +1,25 @@
-use std::cell::{Cell, Ref, RefCell};
 use serde_json;
 use std::sync::{Arc, Condvar};
-use shared_mutex::SharedMutex;
 use std::time;
 use dashmap::DashMap;
+use shared_mutex::SharedMutex;
 
-struct Client {
+struct Client<'a> {
     host_address: String,
-    attack_data: Arc<(SharedMutex<(bool, String)>, Condvar)>,
-    worker_threads: Vec<std::thread::JoinHandle<()>>,
+    attack_data: Arc<(SharedMutex<(bool, &'a str)>, Condvar)>,
     results: Arc<DashMap<u32, Vec<String>>>,
-    thread_count: usize,
 }
 
-
-
-
 //client methods
-impl Client {
+impl Client<'_> {
     //constructor
-    fn new(host: String) -> Client {
+    fn new<'a>(host: String) -> Client<'a> {
         let thread_count = std::thread::available_parallelism().unwrap().get();
         let mut cl = Client {
             host_address: host,
-            attack_data:Arc::new((SharedMutex::new((false, String::new())), Condvar::new())),
-            worker_threads: vec![],
+            attack_data: Arc::new((SharedMutex::new((false, "")), Condvar::new())),
             results: Arc::new(DashMap::with_capacity(thread_count)),
-            thread_count,
         };
-        cl.worker_threads.reserve(thread_count);
         for i in 0..thread_count {
             cl.results.insert(i as u32, vec![]);
         }
@@ -36,13 +27,13 @@ impl Client {
     }
 
     //checks if the server can attack, if it can, it notifies the workers
-    fn can_attack(&self){
+    fn can_attack(&self) {
         // sends request to the server to check if it can attack
         let url = self.host_address.as_str();
         //parses the result
         let req = reqwest::blocking::get(format!("{}/api/attack", url)).unwrap().json::<String>().unwrap().
             strip_prefix("{\"").unwrap().to_string().strip_suffix("\"}").unwrap().to_string();
-        let attack_address_url = req.split("\":\"").collect::<Vec<&str>>()[1].to_string();
+        let attack_address_url = req.split("\":\"").collect::<Vec<&str>>()[1];
         let mut can_attack = !attack_address_url.is_empty();
         let (lock, cv) = &*self.attack_data;
         let mut wlock = lock.write().unwrap();
@@ -55,53 +46,59 @@ impl Client {
         };
     }
 
-    fn start_requesting(&self, client: reqwest::blocking::Client, id:u32, address: &str){
-            let result = client.get(address).send();
-            return match result {
-                Ok(_) => {
-                    let res = result.unwrap();
-                    let code = res.status().as_u16().to_string();
-                    let body = res.text().unwrap();
-                    let response_body = serde_json::json!({"code": code, "body":body});
-                    let local_instance = self.results.clone();
-                    local_instance.get_mut(&id).unwrap().push(response_body.to_string());
-                }
-                Err(_) => {}
-            };
+    fn start_requesting(client: reqwest::blocking::Client, id: u32, address: &str, results: Arc<DashMap<u32, Vec<String>>>, ) {
+        let result = client.get(address).send();
+        return match result {
+            Ok(_) => {
+                let res = result.unwrap();
+                let code = res.status().as_u16().to_string();
+                let body = res.text().unwrap();
+                let response_body = serde_json::json!({"code": code, "body":body});
+                let local_instance = results.clone();
+                local_instance.get_mut(&id).unwrap().push(response_body.to_string());
+            }
+            Err(_) => {}
+        };
     }
 
-    fn thread_worker(&self, id: u32) {
+    fn thread_worker(attack_data: Arc<(SharedMutex<(bool, &str)>,  Condvar)>, results: Arc<DashMap<u32, Vec<String>>>, id: u32) {
         let client = reqwest::blocking::Client::new();
-        let (lock, cv) = &*self.attack_data;
+        let (lock, cv) = &*attack_data;
         //infinite loop todo make it stoppable by some signal
         loop {
-            //locks the mutex (no need to check if it's ok)
             let mut pair = lock.read().unwrap();
             let (attacking, address) = &*pair;
-            //sleep if the worker is not attacking
+            let addr = address.clone().to_string().to_owned();
             if !attacking {
                 pair = pair.wait_for_read(cv).unwrap();
             }
             let mut _r = 0;
             for _ in 0..100 {
-                self.start_requesting(client.clone(), id, address.as_str());
+                Client::start_requesting(client.clone(), id, addr.as_str(), results.clone());
             }
         }
     }
-
+}
 
     //main function, starts the threads and the attack and collects info
-    fn run(mut self) {
-        for i in 0..self.thread_count {
-            //clones the smart pointer to have it's own copy, and sends it to the thread
+    fn run() {
+        let cl = Client::new(String::from("http://localhost:8080"));
+        let attack_data = cl.attack_data.clone();
+        let results = cl.results.clone();
+        let mut worker_threads: Vec<std::thread::JoinHandle<()>> = vec![];
+        let thread_count = std::thread::available_parallelism().unwrap().get();
+        worker_threads.reserve(thread_count);
+        for i in 0..thread_count {
             let ind = i.to_owned().clone();
-            self.worker_threads.push(std::thread::spawn(move || {
-                ();
+            let a = attack_data.clone().to_owned();
+            let r = results.clone().to_owned();
+            worker_threads.push(std::thread::spawn(move || {
+                Client::thread_worker(a, r,ind as u32);
             }));
         }
-        self.can_attack();
-        let addr = self.host_address.to_owned().clone();
-        let armap = self.results.to_owned().clone();
+        cl.can_attack();
+        let addr = cl.host_address.to_owned().clone();
+        let armap = cl.results.to_owned().clone();
         let handl = std::thread::spawn(move || {
             let time = time::Duration::from_millis(950);
             let cl = reqwest::blocking::Client::new();
@@ -128,16 +125,14 @@ impl Client {
         // }
         //waits for the thread to finish todo make it finishable by some signal
         handl.join().unwrap();
-        while self.worker_threads.len() > 0 {
-            let thread = self.worker_threads.pop().unwrap();
+        while worker_threads.len() > 0 {
+            let thread = worker_threads.pop().unwrap();
             thread.join().unwrap();
         }
     }
-}
 
 
 #[no_mangle]
 pub extern "C" fn startup() {
-    let cl = Client::new(String::from("http://localhost:8080"));
-    cl.run();
+    run();
 }
