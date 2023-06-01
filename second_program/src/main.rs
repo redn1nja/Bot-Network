453caf77e507a6e0bda5bf74749ee7e5765d326c
Taras Yaroshko <taras.yaroshko@ucu.edu.ua>
2023-06-01 15:50:51 +0300

    Start communication for lib reloading

--- second_program/src/main.rs
+++ second_program/src/main.rs
@@ -1,11 +1,43 @@
-use libloading::{Library, Symbol};
-fn main() {
-    unsafe {
-        loop {
-            let lib = Library::new("../src/client/target/debug/libclient_lib.so").expect("Failed to load dynamic library");
-            let main_fun: Symbol<extern "C" fn()->bool> = lib.get(b"startup").expect("Failed to load function");
-            let result = main_fun();
-        }
+use std::env::consts::OS;
 
+use libloading::{Library, Symbol};
+
+fn main() {
+    let mut LIB_EXTENSION = "";
+    match OS {
+        "macos" => {
+            LIB_EXTENSION = "dylib";
+        }
+        "windows" => {
+            LIB_EXTENSION = "dll";
+        }
+        "linux" => {
+            LIB_EXTENSION = "so";
+        }
+        _ => {
+            panic!("Unsupported OS");
+        }
+    }
+
+    let mut library_path = format!("../src/client/target/debug/libclient_lib.{}", LIB_EXTENSION);
+
+    loop {
+        unsafe {
+            let lib = Library::new(&library_path).expect("Failed to load dynamic library");
+            let main_fun: Symbol<extern "C" fn() -> bool> =
+                lib.get(b"startup").expect("Failed to load function");
+            let result = main_fun();
+
+            let new_library_path = format!("/tmp/libclient_lib.{}", LIB_EXTENSION);
+            if std::path::Path::new(&new_library_path).exists() {
+                lib.get::<Symbol<extern "C" fn()>>(b"shutdown")
+                    .expect("Failed to get shutdown function")();
+                drop(lib);
+
+                library_path = new_library_path.to_owned();
+
+                std::fs::remove_file(&new_library_path).expect("Failed to remove new library file");
+            }
+        }
     }
 }
--- src/client/src/lib.rs
+++ src/client/src/lib.rs
@@ -1,13 +1,14 @@
-use std::sync::{Arc, Condvar};
-use std::sync::atomic::{AtomicBool, Ordering};
 use dashmap::DashMap;
 use serde_json;
 use shared_mutex::{SharedMutex, SharedMutexWriteGuard};
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::{Arc, Condvar};
 
 struct Client {
     host_address: String,
     attack_data: Arc<(SharedMutex<String>, Condvar)>,
     results: Arc<DashMap<u32, Vec<String>>>,
+    exit_signal: Arc<AtomicBool>,
 }
 
 // Client methods
@@ -25,6 +26,7 @@ impl Client {
             host_address: host,
             attack_data: Arc::new((SharedMutex::new(String::new()), Condvar::new())),
             results: Arc::new(DashMap::with_capacity(thread_count)),
+            exit_signal: Arc::new(AtomicBool::new(false)),
         };
         for i in 0..thread_count {
             cl.results.insert(i as u32, vec![]);
@@ -32,24 +34,34 @@ impl Client {
         cl
     }
 
+    #[no_mangle]
+    pub extern "C" fn shutdown() {
+        std::process::exit(0);
+    }
 
     // Checks if the server can attack, if it can, it notifies the workers
     fn can_attack<'a>(&self) {
-
         let url = self.host_address.as_str();
-        let req = reqwest::blocking::get(format!("{}/api/attack", url)).unwrap().json::<String>().unwrap().
-            strip_prefix("{\"").unwrap().to_string().strip_suffix("\"}").unwrap().to_string();
+        let req = reqwest::blocking::get(format!("{}/api/attack", url))
+            .unwrap()
+            .json::<String>()
+            .unwrap()
+            .strip_prefix("{\"")
+            .unwrap()
+            .to_string()
+            .strip_suffix("\"}")
+            .unwrap()
+            .to_string();
         let mut attack_address_url = req.split("\":\"").collect::<Vec<&str>>()[1].to_string();
         let url_str = attack_address_url.as_str().clone().to_owned();
         let (lock, cv) = &*self.attack_data.clone();
         let mut wlock = lock.write().unwrap();
         *wlock = url_str;
-        drop (wlock);
-        if !attack_address_url.is_empty(){
+        drop(wlock);
+        if !attack_address_url.is_empty() {
             cv.notify_all();
         }
     }
-
 
     fn start_requesting(
         client: reqwest::blocking::Client,
@@ -64,7 +76,10 @@ impl Client {
                 let body = res.text().unwrap_or_default();
                 let response_body = serde_json::json!({"code": code, "body":body});
                 let local_instance = results.clone();
-                local_instance.get_mut(&id).unwrap().push(response_body.to_string());
+                local_instance
+                    .get_mut(&id)
+                    .unwrap()
+                    .push(response_body.to_string());
             }
             Err(err) => {
                 eprintln!("Request error: {}", err);
@@ -97,85 +112,84 @@ impl Client {
     }
 }
 
-
 fn run() {
-    let cl = Client::new(String::from("http://localhost:8080"));
-    let attack_data = cl.attack_data.clone();
-    let results = cl.results.clone();
-    // let stop_signal = Arc::new(AtomicBool::new(false));
-    // let stop_signal_clone = Rc::clone(&stop_signal);
-    let mut worker_threads: Vec<std::thread::JoinHandle<()>> = Vec::new();
-    let thread_count = match std::thread::available_parallelism() {
-        Ok(tp) => tp.get(),
-        Err(_) => {
-            eprintln!("Failed to get thread count. Defaulting to 1 thread.");
-            1
+        let cl = Client::new(String::from("http://localhost:8080"));
+        let attack_data = cl.attack_data.clone();
+        let results = cl.results.clone();
+        let exit_signal = cl.exit_signal.clone(); // Add exit signal clone
+        let mut worker_threads: Vec<std::thread::JoinHandle<()>> = Vec::new();
+        let thread_count = match std::thread::available_parallelism() {
+            Ok(tp) => tp.get(),
+            Err(_) => {
+                eprintln!("Failed to get thread count. Defaulting to 1 thread.");
+                1
+            }
+        };
+        worker_threads.reserve(thread_count);
+        for i in 0..thread_count {
+            let ind = i.clone();
+            let a = attack_data.clone();
+            let r = results.clone();
+            let s = Arc::new(AtomicBool::new(false));
+            worker_threads.push(std::thread::spawn(move || {
+                Client::thread_worker(a, r, ind as u32, s);
+            }));
         }
-    };
-    worker_threads.reserve(thread_count);
-    for i in 0..thread_count {
-        let ind = i.clone();
-        let a = attack_data.clone();
-        let r = results.clone();
-        let s = Arc::new(AtomicBool::new(false));
-        worker_threads.push(std::thread::spawn(move || {
-            Client::thread_worker(a, r, ind as u32, s);
-        }));
-    }
-    cl.can_attack();
-    let addr = cl.host_address.clone();
-    let armap = cl.results.clone();
-    let handl = std::thread::spawn(move || {
-        let time = std::time::Duration::from_millis(950);
-        let cl = reqwest::blocking::Client::new();
-        let map = &*armap;
-        let mut total_request_time: u128 = 0;
-        let mut request_count = 0;
-        loop {
-            std::thread::sleep(time);
-            let mut request_body_vec = Vec::new();
-            for el in map.iter() {
-                request_body_vec.extend(el.value().to_owned());
-            }
-            let throughput = request_body_vec.len();
-            if throughput > 0 {
-                let average_request_time = total_request_time as f64 / request_count as f64;
-                println!("Throughput: {}, Average Request Time: {:.2} ms", throughput, average_request_time);
-            }
-            if request_body_vec.len() >= 20 {
-                let request_builder = cl.post(format!("{}/attack_info", addr.clone()));
-                let start_time = std::time::Instant::now();
-                match request_builder.json(&request_body_vec).send() {
-                    Ok(_) => {
-                        total_request_time += start_time.elapsed().as_millis();
-                        request_count += 1;
-                    }
-                    Err(err) => {
-                        eprintln!("Failed to send attack info: {}", err);
-                    }
-                }
-                map.alter_all(|_, mut v| {
-                    v.clear();
-                    v
-                });
-            }
-        }
-    });
-    // Checks if the server can attack
-    loop {
         cl.can_attack();
-        // if stop_signal_clone.load(Ordering::Relaxed) {
-        //     break;
-        // }
-    }
-    // stop_signal..store(true, Ordering::Relaxed);
-    // Waits for the thread to finish
-    handl.join().unwrap();
-    for thread in worker_threads {
+        let addr = cl.host_address.clone();
+        let armap = cl.results.clone();
+        let handl = std::thread::spawn(move || {
+            let time = std::time::Duration::from_millis(950);
+            let cl = reqwest::blocking::Client::new();
+            let map = &*armap;
+            let mut total_request_time: u128 = 0;
+            let mut request_count = 0;
+            loop {
+                std::thread::sleep(time);
+                let mut request_body_vec = Vec::new();
+                for el in map.iter() {
+                    request_body_vec.extend(el.value().to_owned());
+                }
+                let throughput = request_body_vec.len();
+                if throughput > 0 {
+                    let average_request_time = total_request_time as f64 / request_count as f64;
+                    println!(
+                        "Throughput: {}, Average Request Time: {:.2} ms",
+                        throughput, average_request_time
+                    );
+                    println!("NEW VERSION!!!!!!!!!!");
+                }
+                if request_body_vec.len() >= 20 {
+                    // let request_builder = cl.post(format!("{}/attack_info", addr.clone()));
+                    let start_time = std::time::Instant::now();
+                    cl.post(format!("{}/attack_info", addr.to_owned().clone()))
+                        .json(&request_body_vec)
+                        .send()
+                        .unwrap();
+                    total_request_time += start_time.elapsed().as_millis();
+                    request_count += 1;
+                    map.alter_all(|_, mut v| {
+                        v.clear();
+                        v
+                    });
+                }
+            }
+        });
+        // Checks if the server can attack
+        loop {
+            cl.can_attack();
+            if exit_signal.load(Ordering::Relaxed) {
+                break;
+            }
+        }
+        // stop_signal..store(true, Ordering::Relaxed);
+        // Waits for the thread to finish
+        exit_signal.store(true, Ordering::Relaxed); // Set exit signal to stop the threads
+        handl.join().unwrap();
+        for thread in worker_threads {
         thread.join().unwrap();
-    }
+        }
 }
-
 
 
 #[no_mangle]
