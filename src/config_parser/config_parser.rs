cfb58b6bc2d8613153300658cf302738a2203738
Taras Yaroshko <taras.yaroshko@ucu.edu.ua>
2023-06-01 15:59:02 +0300

    Add sections for config

--- src/config_parser/config_parser.rs
+++ src/config_parser/config_parser.rs
@@ -3,14 +3,23 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
-pub struct ConfigParser {}
+pub struct ConfigParser {
+    pub target_address: Option<String>,
+    pub host_name: Option<String>,
+    pub host_password: Option<String>,
+}
 
 impl ConfigParser {
-    pub fn read_config_file(path: &str) -> Result<HashMap<String, String>, io::Error> {
+    pub fn read_config_file(path: &str) -> Result<ConfigParser, io::Error> {
         let file = File::open(path)?;
         let reader = io::BufReader::new(file);
 
-        let mut config = HashMap::new();
+        let mut config = ConfigParser {
+            target_address: None,
+            host_name: None,
+            host_password: None,
+        };
+
         let mut section = String::new();
 
         for line in reader.lines() {
@@ -23,11 +32,20 @@ impl ConfigParser {
                 let key = line[..index].trim().to_owned();
                 let value = line[index + 1..].trim().to_owned();
 
-                if !section.is_empty() {
-                    let key = format!("{}.{}", section, key);
-                    config.insert(key, value);
-                } else {
-                    config.insert(key, value);
+                match section.as_str() {
+                    "config" => {
+                        if key == "target_address" {
+                            config.target_address = Some(value);
+                        }
+                    }
+                    "credentials" => {
+                        if key == "host_name" {
+                            config.host_name = Some(value);
+                        } else if key == "host_password" {
+                            config.host_password = Some(value);
+                        }
+                    }
+                    _ => {}
                 }
             }
         }
